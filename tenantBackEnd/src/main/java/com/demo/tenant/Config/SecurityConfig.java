3d156ac047ae62e34fa19cb4aa068406e171bed7
hisoka7i <rohit7i@outlook.com>
2024-05-12 23:57:24 +0530

    oath2 implmented

--- tenantBackEnd/src/main/java/com/demo/tenant/Config/SecurityConfig.java
+++ tenantBackEnd/src/main/java/com/demo/tenant/Config/SecurityConfig.java
@@ -4,21 +4,51 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
-import static org.springframework.security.config.Customizer.withDefaults;
+
+import java.util.List;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 	@Bean
 	SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
-		return http.authorizeHttpRequests(auth -> {
-			auth.requestMatchers("/login").permitAll();
+		return http.csrf(AbstractHttpConfigurer::disable)
+				.cors(cors->cors.configurationSource(corsConfigurationSource()))
+				.authorizeHttpRequests(auth -> {
+			//using below line we can open end point, where no auth is required to access
+//			auth.requestMatchers("/login").permitAll();
 			auth.anyRequest().authenticated();
-		}).oauth2Login(withDefaults()).
-				formLogin(withDefaults()).
+		}).oauth2Login(oath2->{
+			//here we can add customization for login page
+			//success handler
+			//failure handler
+			
+			
+			//after success, application returns cookie which we can use
+			//we get jsessionId in the cookie
+		}).
 				build();
 		
 	}
+	@Bean
+	CorsConfigurationSource corsConfigurationSource() {
+		CorsConfiguration configuration = new CorsConfiguration();
+		configuration.setAllowedOrigins(List.of("http://localhost:3000"));
+		configuration.addAllowedHeader("*");
+		configuration.addAllowedMethod("*");
+		configuration.setAllowCredentials(true);
+		
+		
+		UrlBasedCorsConfigurationSource urlBasedCorsConfigurationSource = new UrlBasedCorsConfigurationSource();
+		urlBasedCorsConfigurationSource.registerCorsConfiguration("/**", configuration);
+		
+		return urlBasedCorsConfigurationSource;
+	}
+	
 }
